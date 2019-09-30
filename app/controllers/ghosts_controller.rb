fbd640dda10e2524342af7ea046e8bdf27d21f53
Karly Kizewski <kizewski.karly@gmail.com>
2019-09-30 23:34:29 +0000

    ghost create spec test passes

--- app/controllers/ghosts_controller.rb
+++ app/controllers/ghosts_controller.rb
@@ -7,4 +7,15 @@ class GhostsController < ApplicationController
   def new
     @ghost = Ghost.new
   end
+
+  def create
+    @ghost = Ghost.create(ghost_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def ghost_params
+    params.require(:ghost).permit(:person, :eulogy)
+  end
 end
--- spec/controllers/ghosts_controller_spec.rb
+++ spec/controllers/ghosts_controller_spec.rb
@@ -14,4 +14,14 @@ RSpec.describe GhostsController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe "ghosts#create action" do
+    it "should successfully create a new ghost in our database" do
+      post :create, params: { ghost: { person: 'person', eulogy: 'rest in peace' } }
+      expect(response).to redirect_to root_path
+      ghost = Ghost.last
+      expect(ghost.person).to eq('person')
+      expect(ghost.eulogy).to eq('rest in peace')
+    end
+  end
 end
