b6e6323f9b4f201203866a839da9661a41611cd0
Karly Kizewski <kizewski.karly@gmail.com>
2019-10-01 04:07:22 +0000

    validations alterations

--- app/models/ghost.rb
+++ app/models/ghost.rb
@@ -1,4 +1,4 @@
 class Ghost < ApplicationRecord
-  validates :person, presence: true, length: { maximum: 75, minimum: 3 }
-  validates :eulogy, presence: true, length: { maximum: 300, minimum: 3 }
+  validates :person, presence: true, length: { maximum: 100, minimum: 3 }
+  validates :eulogy, presence: true, length: { maximum: 600, minimum: 3 }
 end
